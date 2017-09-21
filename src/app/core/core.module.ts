62543d4cf11729a01607003a25e025a40193a932
challengezwb <challengezwb@gmail.com>
2017-09-21 19:01:51 -0400

    newest theme

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import {NgModule, SkipSelf, Optional} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {SharedModule} from '../shared/shared.module';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 import {CenterComponent} from './main/center.component';
-import {MdToolbarModule} from '@angular/material';
+
 @NgModule({
   imports: [
-    CommonModule, MdToolbarModule,
+    CommonModule,
+    SharedModule,
   ],
   declarations: [
     HeaderComponent,
@@ -17,6 +19,7 @@ import {MdToolbarModule} from '@angular/material';
     HeaderComponent,
     FooterComponent,
     CenterComponent,
+    SharedModule,
   ]
 })
 export class CoreModule {
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SharedModule} from '../../shared/shared.module';
 
 @Component({
   selector: 'app-header',
