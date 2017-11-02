1c4a859ea897900bd5cf7e875564634ca7a19b5a
challengezwb <challengezwb@gmail.com>
2017-11-02 16:09:40 -0400

    2223

--- src/app/order-manage/order-manage.component.ts
+++ src/app/order-manage/order-manage.component.ts
@@ -1,16 +1,18 @@
-import {Component, OnInit} from '@angular/core';
-
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
 @Component({
   selector: 'app-order-manage',
   templateUrl: './order-manage.component.html',
   styleUrls: ['./order-manage.component.css']
 })
 export class OrderManageComponent implements OnInit {
-
+  @Output() toggle = new EventEmitter<void>();
   constructor() {
   }
 
   ngOnInit() {
   }
 
+  openSidebar() {
+    this.toggle.emit();
+  }
 }
