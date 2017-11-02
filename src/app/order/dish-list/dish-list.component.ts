cd5d0b952764cef553ae34cdaedf32708af1dda4
zyn1018 <angelo931018@yahoo.com>
2017-11-02 15:14:19 -0400

    Merge branches 'beta' and 'zyn' of github.com:zyn1018/gator-bite into zyn
    # Conflicts:
    #	src/app/login/login/login.component.html

---
+++ src/app/order/dish-list/dish-list.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-dish-list',
+  templateUrl: './dish-list.component.html',
+  styleUrls: ['./dish-list.component.css']
+})
+export class DishListComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
