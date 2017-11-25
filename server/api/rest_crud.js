a0562f7f21589b8387214f7dc0902e58dec76e43
Peidong Zhu <zhupd1992@gmail.com>
2017-11-25 17:37:53 -0500

    Merge branch 'beta' of https://github.com/zyn1018/gator-bite into beta
    # Conflicts:
    #	server/api/auth.js

---
+++ server/api/rest_crud.js
@@ -0,0 +1,29 @@
+var express = require("express"),
+  router = express.Router(),
+  User = require("../models/user"),
+  Restaurant = require("../models/restaurant");
+var getJwt = require("./getJwt");
+
+
+/**
+ * restaurant profile update
+ * req.json{picture, }
+ */
+router.post("/restUpdate", function (req, res) {
+  var id = getJwt(req);
+  Restaurant.findById(id, function (err, restaurant) {
+    if(err){
+      res.status(400).send(err);
+    }else{
+      restaurant.picture = req.body.picture || restaurant.picture;
+      restaurant.type = req.body.type || restaurant.type;
+      restaurant.delivery_fee = req.body.delivery_fee || restaurant.delivery_fee;
+      restaurant.menu = req.body.menu || restaurant.menu;
+    }
+
+  })
+});
+
+
+
+module.exports = router;
