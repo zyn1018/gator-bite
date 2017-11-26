9b2c849231adfefdb732a98b4014e89fc253f028
zyn1018 <angelo931018@yahoo.com>
2017-11-26 16:21:48 -0500

    Implement function of submitting order

--- src/app/restaurantsList/restaurant.service.ts
+++ src/app/restaurantsList/restaurant.service.ts
@@ -1,10 +1,25 @@
 import {Injectable} from '@angular/core';
 import {Restaurant} from './restaurant';
 import {Restaurants} from './mock-restaurants';
+import {Http} from '@angular/http';
 
 @Injectable()
 export class RestaurantService {
+  public restaurants: Restaurant[];
+
+  constructor(private http: Http) {
+  }
+
   getRestaurants(): Promise<Restaurant[]> {
     return Promise.resolve(Restaurants);
   }
+
+  getRestaurantsDB() {
+    this.http.get('/api/restaurant').subscribe(data => {
+      // console.log(data.json());
+      localStorage.setItem('restaurantList', JSON.stringify(data.json()));
+    });
+
+  }
 }
+
